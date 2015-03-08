c7cf04485d60a5bd52936007b5992564add49c7d
arif <arif@adplus.co.id>
2015-03-08 21:59:30 +0700

    controller and action route

--- phpsam/mvc/controller.php
+++ phpsam/mvc/controller.php
@@ -8,7 +8,12 @@ class controller {
     function action($action_name=null,$action_params=null) {
         if($action_name==null) $action_name='index';
         $this->before_action();
-        call_user_func_array(array($this, 'action_' . $action_name), $action_params);
+        if(method_exists($this, "action_" . $action_name)){
+            $x=call_user_func_array(array($this, 'action_' . $action_name), $action_params);
+        }
+        else {
+            \phpsam\route\route::throw_error(404);
+        }
         $this->after_action();
     }
     
