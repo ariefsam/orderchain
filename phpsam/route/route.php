e13b57361357edd4644b5f20517207c1f02e7ada
arif <arif@adplus.co.id>
2015-03-08 21:37:46 +0700

    route fix

--- phpsam/route/route.php
+++ phpsam/route/route.php
@@ -7,11 +7,16 @@ class route {
         if($server==null) {
             $server=$_SERVER;
         }
-        print_r($server);
         $base_url=$server['HTTP_HOST'].dirname($server['SCRIPT_NAME']);
         \phpsam::$base_url=$base_url;
+        $uri=  str_replace(dirname($server['SCRIPT_NAME']).'/', "", $server['REQUEST_URI']);
         
-        $controller=new \mvc\controller\home();
+        $explode_url=  explode("/", $uri);
+        if(!$controller_name=@$explode_url[0]) {
+            $controller_name='home';
+        }
+        $controller_class="\\mvc\\controller\\" . $controller_name;
+        $controller=new $controller_class();
         $controller->action('index',array());
     }
     
